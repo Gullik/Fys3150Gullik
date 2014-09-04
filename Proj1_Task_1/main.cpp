79d86e8a99775be1e1df872166b0bbb7a9bed18d
Gullik <gullik.killie@gmail.com>
2014-09-04 22:13:01 +0200

    Evening work

--- Proj1_Task_1/main.cpp
+++ Proj1_Task_1/main.cpp
@@ -1,36 +1,53 @@
 #include <iostream>
 #include "armadillo"
 
+
 using namespace std;
 using namespace arma;
 
-void Tridim_Solver(int N)
+vec Tridim_Solver(int N)
 {
-    ivec a = -1*ones<ivec>(N);
-    ivec b = 2*ones<ivec>(N);
-    ivec c = -1*ones<ivec>(N);
+    int a = -1; // The three constants for the three diagonals in the
+    int b = 2;  // matrix
+    int c = -1;
+
+    vec divisor_temp(N);    // Used in the forward subsitution step,
+                            // predefined here to avoid redefining it several times in the for loop
 
     vec u(N);
-
-    cout << u.n_elem << endl;
-
-
-
-
-//    cout << "Vectoren a er " << endl << a  << endl;
-//    cout << "Vectoren b er " << endl << b  << endl;
-//    cout << "Vectoren c er " << endl << c  << endl;
-
     int i;
 
-    for(i=0 ; i<N ; i++)
+    //Creating some points of the source term f(x) = 100e^{-10x}
+    vec f = exp(-10 * linspace(0,1, N));
+
+//    cout << f << endl;
+
+    //The forward subsitution step, which gives u(i) + cst*u(i+1) = cst*f(i)
+
+    double btemp = b;
+    u(0) = f(0)/b;
+
+    for(i=1 ; i < N ; i++)
     {
-
-        cout << "u(" << i << ") = " << u(i) << endl;
-
+        divisor_temp(i) = c/btemp;
+        btemp = (b - (a*divisor_temp(i)));
+        u(i) = (f(i) - (a*u(i-1)))/btemp;
     }
 
-    return;
+    // The backwards step;
+    for ( i = N-2 ; i>=0 ; i--)
+    {
+        u(i) -= divisor_temp(i)*u(i+1);
+    }
+
+
+
+//    for(int i=0 ; i<N ; i++)
+//    {
+//        cout << "u(" << i << ") = " << u(i)   <<endl;
+//    }
+
+    return u;
 }
 
 int main()
@@ -41,7 +58,13 @@ int main()
 
    cin >> N;
 
-   Tridim_Solver(N);
+   vec x = linspace(0,1,N);
+
+   vec u = Tridim_Solver(N);
+
+   vec Exact = 1 - (1-exp(-10))*x - exp(-10*x);
+
+
 
 
 
